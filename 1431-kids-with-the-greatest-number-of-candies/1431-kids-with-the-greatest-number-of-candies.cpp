0dd5198a1a3789b748433ef765e1b0d3df3c5c90
Hardik Bang <hardikbang777@gmail.com>
2026-09-13 09:35:16 +0530

    Time: 0 ms (100%), Space: 12.5 MB (26.14%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& c, int ec) {
+        int maxi=INT_MIN;
+        for(int i=0;i<c.size();i++){
+            maxi=max(maxi,c[i]);
+        }
+        vector<bool> ans;
+        for(int i=0;i<c.size();i++){
+            if(c[i]+ec>=maxi) ans.push_back(1);
+            else ans.push_back(0);
+        }
+        return ans;
+    }
+};
